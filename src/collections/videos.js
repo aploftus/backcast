cdd2c7af44b198b569891d99d0c78538d2c6c505
Hack Reactor Student <student@hackreactor.com>
2017-10-23 17:33:47 -0700

    Render App in browser, pass in example video data

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -3,3 +3,21 @@ var Videos = Backbone.Collection.extend({
   model: Video
 
 });
+
+// var commentList = [
+//   new Comment('Doug!'), // default to 0 votes
+//   new Comment('Doug?', 1),
+//   new Comment('Doug.', 2),
+//   new Comment('"Doug".', 3),
+//   new Comment('Doug?!', 4)];
+// Add all of these to a new collection:
+// var comments = new Comments(commentList);
+  
+// var videoArray = [];
+// exampleVideoData.forEach(function(video) {
+//   videoArray.push(new Video(video));
+// });
+
+var videos = new Videos(exampleVideoData.map(function(video) {
+  return new Video(video);
+}));
