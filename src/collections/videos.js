a31f96971b93ece358dfda4bb3168cdd595aca3b
Hack Reactor Student <student@hackreactor.com>
2017-10-24 17:35:03 -0700

    Add youtube api and search functionality

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -1,5 +1,26 @@
 var Videos = Backbone.Collection.extend({
 
-  model: Video
-
+  model: Video,
+  
+  url: 'https://www.googleapis.com/youtube/v3/search',
+  
+  search: function(query) {
+    
+    this.fetch({
+      data: {
+        q: query,
+        maxResults: 5,
+        key: window.YOUTUBE_API_KEY,
+        type: 'video',
+        videoEmbeddable: 'true',
+        part: 'snippet'
+      },
+      success: function(data) {
+        console.log('we fetched!' + data);
+      },
+      failure: function() {
+        console.log('sorry, we were unable to fetch');
+      }
+    });
+  }
 });
--- src/views/app.js
+++ src/views/app.js
@@ -3,7 +3,8 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos(window.exampleVideoData);
+    this.videos = new Videos();
+    this.videos.search('bunnies');
     this.videoListView = new VideoListView({ collection: this.videos });
     this.videoPlayerView = new VideoPlayerView({model: this.videos.at(0), collection: this.videos});
     this.render();
